01056036b036442d9c65f6709661deba41d9307f
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-09-14 12:26:56 -0300

    feat(TestDomain): add TestDomain & some cases

---
+++ FinTrac/TestDomain/TestUser.cs
@@ -0,0 +1,45 @@
+﻿namespace TestDomain
+{
+    [TestClass]
+    public class TestUser
+    {
+        [TestMethod]
+        public void createUser()
+        {
+        }
+
+        [TestMethod]
+        // CRUD method?
+        public void createUserAlreadyExists()
+        {
+        }
+
+        //Pass less 10 characters
+        //Pass more 30 characters
+
+        [TestMethod]
+        public void modifyName()
+        {
+        }
+
+        [TestMethod]
+        public void modifyLastName()
+        {
+        }
+
+        [TestMethod]
+        public void modifyDirection()
+        {
+        }
+
+        [TestMethod]
+        public void modifyEmail()
+        {
+        }
+
+        [TestMethod]
+        public void modifyPassword()
+        {
+        }
+    }
+}
