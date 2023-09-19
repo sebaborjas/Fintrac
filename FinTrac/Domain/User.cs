8c8afc4f70fd0e589e7e64fc5ff8d235b207e84d
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-09-19 15:11:37 -0300

    feat(TestUser): add test user cases

---
+++ FinTrac/Domain/User.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class User
+    {
+        private string _name;
+        private string _email;
+        private string _password;
+        public string Name {
+            get
+            {
+                return _name;
+            }
+            set 
+            {
+                if (value == "") 
+                {
+                    throw new ArgumentException("No se admiten campos vacíos");
+                } else
+                {
+                    _name = value;
+                }
+            } 
+        }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { 
+            get 
+            { 
+                return _email; 
+            } 
+            set 
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("No se adminten campos vacios");
+                } else
+                {
+                    _email = value;
+                }
+            } }
+        public string Password {
+            get 
+            {
+                return _password;
+            }
+            set
+            {
+                if(value == "")
+                {
+                    throw new ArgumentException("No se admiten campos vacios");
+                } else
+                {
+                    _password = value;
+                }
+            } 
+        }
+    }
+}
--- FinTrac/TestDomain/TestUser.cs
+++ FinTrac/TestDomain/TestUser.cs
@@ -1,45 +1,95 @@
-﻿namespace TestDomain
+﻿using Domain;
+namespace TestDomain
 {
     [TestClass]
     public class TestUser
     {
-        [TestMethod]
-        public void createUser()
+        User user;
+
+        [TestInitialize]
+        public void Setup()
         {
+            user = new User();
+        }
+
+        [TestMethod]
+        public void CreateUser()
+        {
+            User u2 = new User();
+            Assert.AreEqual(u2, u2);
         }
 
         [TestMethod]
         // CRUD method?
-        public void createUserAlreadyExists()
-        {
-        }
-
-        //Pass less 10 characters
-        //Pass more 30 characters
-
-        [TestMethod]
-        public void modifyName()
+        public void CreateUserAlreadyExists()
         {
         }
 
         [TestMethod]
-        public void modifyLastName()
+        public void ModifyName()
         {
+            user.Name = "Test";
+            Assert.AreEqual("Test", user.Name);
         }
 
         [TestMethod]
-        public void modifyDirection()
+        public void ModifyLastName()
         {
+            user.LastName = "Marotta";
+            Assert.AreEqual("Marotta", user.LastName);
         }
 
         [TestMethod]
-        public void modifyEmail()
+        public void ModifyAddress()
         {
+            user.Address = "New Address";
+            Assert.AreEqual("New Address", user.Address);
+
         }
 
         [TestMethod]
-        public void modifyPassword()
+        public void ModifyEmail()
         {
+            user.Email = "New Email";
+            Assert.AreEqual("New Email", user.Email);
+        }
+
+        [TestMethod]
+        public void ModifyPassword()
+        {
+            user.Password = "New Password";
+            Assert.AreEqual("New Password", user.Password);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyEmptyNameException()
+        {
+            String newName = "";
+            user.Name = newName;
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyEmptyEmailException()
+        {
+            String newMail = "";
+            user.Email = newMail;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ModifyEmptyPasswordException()
+        {
+            String newPassword = "";
+            user.Password = newPassword;
+        }
+
+        [TestMethod]
+        public void PasswordBetweenTenAndTwentyCharacters()
+        {
+            bool correctLength = user.Password.Length > 9 && user.Password.Length < 20;
+            Assert.IsTrue(correctLength);
         }
     }
 }
