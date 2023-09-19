21c31385462ff05b1ada1df15270ad08f0ddba56
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-09-19 18:28:16 -0300

    feat(TestUser): refactor custom excpetion

---
+++ FinTrac/Domain/EmptyFieldException.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Serialization;
+
+namespace Domain
+{
+    [Serializable]
+    public class EmptyFieldException : Exception
+    {
+        public static string Message = "No se admiten campos vacios";
+
+        public EmptyFieldException() : this(Message){
+
+        }
+ 
+        private EmptyFieldException(string? message) : base(message)
+        {
+
+        }
+
+    }
+}
