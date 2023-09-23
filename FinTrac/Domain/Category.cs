54f1af0f8bd528b286e536b4497f4347991568bf
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-09-22 22:55:49 -0300

    feat(Category): refactor properties

--- FinTrac/Domain/Category.cs
+++ FinTrac/Domain/Category.cs
@@ -3,15 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using Domain.DataTypes;
+using Domain.Excepciones;
 
 namespace Domain
 {
 	public class Category
 	{
-		private string name;
-		private DateTime creationDate;
-		private CategoryStatus status;
-		private CategoryType type;
+		private string _name;
+		private DateTime _creationDate;
+		private CategoryType _type;
+		private CategoryStatus _status;
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+			set
+			{
+				if (value == "")
+				{
+					throw new EmptyFieldException();
+				}
+				_name = value;
+				
+			}
+		}
+		public DateTime CreationDate {
+			get
+			{
+				return _creationDate;
+			}
+			set
+			{
+				if (value < DateTime.Today)
+				{
+					throw new ArgumentException();
+				}
+				if (value > DateTime.Today)
+				{
+					throw new ArgumentException();
+				}
+				_creationDate = value;
+
+			}
+		}
+		public CategoryType Type
+		{
+			get
+			{
+				return _type;
+			}
+			set
+			{
+				_type = value;
+			}
+		}
+		public CategoryStatus Status
+		{
+			get
+			{
+				return _status;
+			}
+			set
+			{
+				_status = value;
+			}
+		}
+
 	}
 }
