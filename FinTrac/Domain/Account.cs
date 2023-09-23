8b16ec069465fd4a7645e98bd275aa348b1ab9b8
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-09-22 21:20:28 -0300

    feat(account): add class account

--- FinTrac/Domain/Account.cs
+++ FinTrac/Domain/Account.cs
@@ -1,7 +1,14 @@
-﻿namespace Domain
+﻿using Domain.DataTypes;
+
+namespace Domain
 {
-    public class Class1
+    public abstract class Account
     {
+        public string Name { get; set; }
+        public DateTime CreationDate { get; set; }
+        public Currency Currency { get; set; }
+
+        /*public WorkSpace workSpace { get; set; }*/
 
     }
 }
