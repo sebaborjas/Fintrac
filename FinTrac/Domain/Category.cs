e1f1bb28734a24b2e9ea57d6b475cf6055c8bbf6
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-09-22 22:19:01 -0300

    feat(Category): add class Category

---
+++ FinTrac/Domain/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DataTypes;
+
+namespace Domain
+{
+	public class Category
+	{
+		private string name;
+		private DateTime creationDate;
+		private CategoryStatus status;
+		private CategoryType type;
+	}
+}
