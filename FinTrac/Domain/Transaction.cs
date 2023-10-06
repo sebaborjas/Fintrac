dc351c4f77afc583c2469cdc9553cd96ffbd1c23
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-10-06 18:38:16 -0300

    feat(TestTransaction): add transaction

---
+++ FinTrac/Domain/Transaction.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Domain.Excepciones;
+
+namespace Domain
+{
+	public class Transaction{
+
+		private string _title;
+		private DateTime _creationDate;
+		private double _amount;
+		private string _currency;
+		private Category _category;
+		private string _account;
+
+		public string Title
+		{
+			get => _title;
+			set
+			{
+				if (value == "")
+				{
+					throw new EmptyFieldException();
+				}
+				else
+				{
+					_title = value;
+				}
+			}
+		}	
+		public DateTime CreationDate
+		{
+			get => _creationDate;
+			set => _creationDate = value;
+		}
+		public double Amount
+		{
+			get => _amount;
+			set
+			{
+				if (value < 0)
+				{
+					throw new Exception();
+				}
+				else
+				{
+					_amount = value;
+				}
+			}
+		}
+
+		public string Currency
+		{
+			get => _currency;
+			set => _currency = value;
+		}
+
+		public Category Category
+		{
+			get => _category;
+			set => _category = value;
+		}
+		public string Account
+		{
+			get => _account;
+			set => _account = value;
+		}
+		
+		public Transaction()
+		{
+		}
+
+	}
+}
--- FinTrac/TestDomain/TestTransaction.cs
+++ FinTrac/TestDomain/TestTransaction.cs
@@ -46,20 +46,6 @@ namespace TestDomain
 			transaction.Amount = 100;
 			Assert.AreEqual(100, transaction.Amount);
 		}
-
-		//[TestMethod]
-		//[ExpectedException(typeof(ArgumentException))]
-		//public void TestAddNegativeAmount()
-		//{
-		//	transaction.Amount = -100;
-		//}
-
-		/*[TestMethod]
-		[ExpectedException(typeof(ArgumentException))]
-		public void TestAddZeroAmount()
-		{
-			transaction.Amount = 0;
-		}
 */
 		[TestMethod]
 		public void TestAddDecimalAmount()
@@ -67,29 +53,5 @@ namespace TestDomain
 			transaction.Amount = 100.5;
 			Assert.AreEqual(100.5, transaction.Amount);
 		}
-
-		/*
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentException))]
-		public void TestAddNegativeDecimalAmount()
-		{
-			transaction.Amount = -100.5;
-		}
-
-
-		[TestMethod]
-		public void TestAddCurrency()
-		{
-			transaction.Currency = "UYU";
-			Assert.AreEqual("UYU", transaction.Currency);
-		}
-
-		[TestMethod]
-		[ExpectedException(typeof(ArgumentException))]
-		public void TestAddInvalidCurrency()
-		{
-			transaction.Currency = "UY";
-		}
-		*/
 	}
 }
