5e5a7cf3c7ba80a943596e6242a6a9613119ef42
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-10-06 19:30:42 -0300

    refactor(Transaction): add Transaction properties

--- FinTrac/Domain/Transaction.cs
+++ FinTrac/Domain/Transaction.cs
@@ -6,6 +6,7 @@ using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
+using Domain.DataTypes;
 using Domain.Excepciones;
 
 namespace Domain
@@ -13,11 +14,12 @@ namespace Domain
 	public class Transaction{
 
 		private string _title;
-		private DateTime _creationDate;
+		private DateTime _creationDate = DateTime.Today;
 		private double _amount;
-		private string _currency;
-		private Category _category;
-		private string _account;
+		private CurrencyType _currency;
+		private CategoryType _category;
+		private Object _account;
+		private Object _workSpace;
 
 		public string Title
 		{
@@ -33,11 +35,21 @@ namespace Domain
 					_title = value;
 				}
 			}
-		}	
+		}
 		public DateTime CreationDate
 		{
 			get => _creationDate;
-			set => _creationDate = value;
+			set
+			{
+				if (value > DateTime.Today)
+				{
+					throw new ArgumentException("La fecha es invalida");
+				}
+				else
+				{
+					_creationDate = value;
+				}
+			}
 		}
 		public double Amount
 		{
@@ -55,26 +67,69 @@ namespace Domain
 			}
 		}
 
-		public string Currency
+		public CurrencyType Currency
 		{
 			get => _currency;
-			set => _currency = value;
+			set
+			{
+				if (value == CurrencyType.UYU || value == CurrencyType.USD)
+				{
+					_currency = value;
+				}
+				else
+				{
+					throw new Exception();
+				}
+				
+			}
 		}
 
-		public Category Category
+		public CategoryType Category
 		{
 			get => _category;
-			set => _category = value;
-		}
-		public string Account
-		{
-			get => _account;
-			set => _account = value;
-		}
-		
-		public Transaction()
-		{
+			set
+			{
+				if (value == CategoryType.Cost || value == CategoryType.Income)
+				{
+					_category = value;
+				}
+				else
+				{
+					throw new Exception();
+				}
+			}
 		}
 
+		public Object Account
+		{
+			get
+			{
+				return _account;
+			}
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException("No se permiten cuentas vacias");
+				}
+				_account = value;
+			}
+		}
+
+		public Object WorkSpace
+		{
+			get
+			{
+				return _workSpace;
+			}
+			set
+			{
+				if (value == null)
+				{
+					throw new ArgumentNullException("No se permiten espacios de trabajo vacios");
+				}
+				_workSpace = value;
+			}
+		}
 	}
 }
