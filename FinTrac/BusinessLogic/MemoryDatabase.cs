a10287053dbd240e4fe71c00b471dec26f330fc9
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-07 17:19:35 -0300

    feat(MemoryDatabase): add memory database class

---
+++ FinTrac/BusinessLogic/MemoryDatabase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic.Interfaces;
+
+namespace BusinessLogic
+{
+	public class MemoryDatabase : IRepository
+	{
+		public void Add<T>(T entity) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete<T>(T entity) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public T Get<T>(int id) where T : class
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update<T>(T entity) where T : class
+		{
+			throw new NotImplementedException();
+		}
+	}	
+	{
+	}
+}
