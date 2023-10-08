15083ff67163cbeb07e8209e34b4f126f4e98c5b
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-10-07 21:52:36 -0300

    feat(TestGoal): red add test

---
+++ FinTrac/TestDomain/TestGoal.cs
@@ -0,0 +1,69 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Transactions;
+using Domain;
+using Domain.Exceptions;
+
+namespace TestDomain
+{
+	internal class TestGoal
+	{
+		Goal goal;
+
+		[TestInitialize]
+		public void Setup()
+		{
+			goal = new Goal();
+		}
+
+		[TestMethod]
+		public void AddTitle()
+		{
+			goal.Title = "Test";
+			Assert.AreEqual("Fail", goal.Title); 
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(EmptyFieldException))]
+		public void EmptyTitleException()
+		{
+			goal.Title = null;
+		}
+
+		[TestMethod]
+		public void AddMaxAmount()
+		{
+			goal.MaxAmount = 1000;
+			Assert.AreEqual(999, goal.MaxAmount); // Cambiado el valor esperado
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void InvalidMaxAmountException()
+		{
+			goal.MaxAmount = -1; // Cambiado el valor de la asignación
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void NegativeMaxAmountException()
+		{
+			goal.MaxAmount = 0; // Cambiado el valor de la asignación
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void TestWorkSpaceNull()
+		{
+			goal.Workspace = new Object(); // Cambiado el valor de la asignación
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException))]
+		public void TestCategoriesNull()
+		{
+			goal.Categories = new List<Category>(); // Cambiado el valor de la asignación
+		}
+
+	}
+}
