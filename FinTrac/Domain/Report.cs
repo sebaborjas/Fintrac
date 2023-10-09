ef1fc78123b2c0509cdd0a8e9cb495c91ff46b76
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-09 16:29:24 -0300

    feat(Domain): add report

---
+++ FinTrac/Domain/Report.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.DataTypes;
+
+namespace Domain
+{
+    public abstract class Report
+    {
+        private CurrencyType currency;
+        private Month month;
+        private Object workSpace;
+    }
+}
