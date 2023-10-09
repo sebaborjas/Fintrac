9249817963ac79fd874422b8e4bd0e7036eabcb4
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-09 17:33:55 -0300

    feat(PersonalAccountBalance): [RED] personalAccountBalance

---
+++ FinTrac/Domain/AccountBalanceReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class AccountBalanceReport : Report
+    {
+        public PersonalAccount Account { get; set; }
+
+        public double CalculateBalance()
+        {
+            return 1;
+        }
+    }
+}
---
+++ FinTrac/TestDomain/TestAccountBalanceReport.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace TestDomain
+{
+    [TestClass]
+    public class TestAccountBalanceReport
+    {
+        AccountBalanceReport accountBalanceReport;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            PersonalAccount a = new PersonalAccount{ StartingAmount = 0 };
+            accountBalanceReport = new AccountBalanceReport { Account = a };
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void NotWorkspaceException()
+        {
+            accountBalanceReport.WorkSpace = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void NotAccountException()
+        {
+            accountBalanceReport.Account = null;
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void CorrectBalanceOperation()
+        {
+            int montoInicial = 500;
+            int ingresos = 300;
+            int costos = 600;
+            int balance = montoInicial + ingresos - costos;
+
+            Assert.AreEqual(balance, accountBalanceReport.CalculateBalance());
+        }
+    }
+}
