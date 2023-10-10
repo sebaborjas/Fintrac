22b633ba72cfd175ea5d6658b066debd95584d96
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-10 01:20:38 -0300

    feat(TestBussinessLogic/AccountService): add unit test names

---
+++ FinTrac/BusinessLogic/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class AccountService
+    {
+        private readonly MemoryDatabase _memorryDatabase;
+
+        public AccountService(MemoryDatabase memorryDatabase)
+        {
+            _memorryDatabase = memorryDatabase;
+        }
+    }
+}
---
+++ FinTrac/TestBusinessLogic/TestAccountService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLogic;
+using Domain.DataTypes;
+using Domain;
+
+namespace TestBusinessLogic
+{
+    [TestClass]
+    public class TestAccountService
+    {
+        private AccountService _service;
+        [TestInitialize]
+        public void SetUp()
+        {
+            _service = new AccountService(new MemoryDatabase());
+        }
+
+        [TestMethod]
+        public void AddPersonalAccount() 
+        {
+        }
+
+        [TestMethod]
+        public void AddCreditCard() { }
+
+        [TestMethod]
+        public void GetAccounts() { }
+
+        [TestMethod]
+        public void ModifyPersonalAccount() { }
+
+        [TestMethod]
+        public void ModifyCreditCard() { }
+
+        [TestMethod]
+        public void DeletePersonalAccount() { }
+
+        [TestMethod]
+        public void DeleteCreditCard() { }
+    }
+}
