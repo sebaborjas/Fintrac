9bb6bcd6545e445aefbbc4664ac3ce8c5f2c2789
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-10 20:14:54 -0300

    refactor(BusinessLogic):

--- FinTrac/BusinessLogic/MemoryDatabase.cs
+++ FinTrac/BusinessLogic/MemoryDatabase.cs
@@ -1,50 +1,4 @@
-<<<<<<< Updated upstream
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using BusinessLogic.Interfaces;
-using Domain;
-
-namespace BusinessLogic
-{
-    public class MemoryDatabase
-    {
-        public bool isLoggedIn { get; set; } = false;
-        public List<User> Users { get; set; } = new List<User>();
-
-        public List<Workspace> Workspaces { get; set; } = new List<Workspace>();
-        public List<Account> Accounts { get; set; } = new List<Account>();
-
-        public List<Workspace> Workspaces { get; set; }
-
-        public MemoryDatabase()
-        {
-            Users = new List<User>();
-            Workspaces = new List<Workspace>();
-            addUsers();
-        }
-     
-        
-        
-        public void addUsers()
-        {
-            User otro = new User
-            {
-                Name = "Emiliano",
-                LastName = "Marotta",
-                Email = "etest@test.com",
-                Password = "1234123412",
-                Address = "123"
-            };
-            Users.Add(otro);
-        }
-    }
-
-}
-=======
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -59,7 +13,8 @@ namespace BusinessLogic
         public bool isLoggedIn { get; set; } = false;
 
         public List<User> Users { get; set; } = new List<User>();
-	      public User currentUser { get; set; }
+	    
+        public User currentUser { get; set; }
 
 
 
@@ -86,4 +41,3 @@ namespace BusinessLogic
     }
 
 }
->>>>>>> Stashed changes
