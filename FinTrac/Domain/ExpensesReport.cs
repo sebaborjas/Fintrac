67a028c3524a89f6aed26b0b9ba3435b0be09c11
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-11 19:39:24 -0300

    feat(Domain): [RED] ExpensesReport

---
+++ FinTrac/Domain/ExpensesReport.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class ExpensesReport
+    {
+        public Workspace Workspace { get; set; }
+
+        public List<Transaction> ListExpenses() {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> ListExpensesByCategory(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> ListExpensesByDate(DateTime fromDate, DateTime toDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> ListExpensesByAccount(string accountName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
