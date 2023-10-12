8699c7187e96faac4d45f862efc22daba9ce75c6
emilianomarotta <emilianomarott@gmail.com>
2023-10-11 23:19:08 -0300

    feature(Business/GoalService): Add TDD ´principal functions

--- FinTrac/TestBusinessLogic/TestGoalService.cs
+++ FinTrac/TestBusinessLogic/TestGoalService.cs
@@ -2,6 +2,7 @@
 using Domain.DataTypes;
 using Domain;
 using Domain.Exceptions;
+using System.Xml;
 
 namespace TestBusinessLogic
 {
@@ -48,9 +49,251 @@ namespace TestBusinessLogic
 
         [TestMethod]
         [ExpectedException(typeof(EmptyCategoryListException))]
-        public void AddGoalCategoriesListEmpty() {
+        public void AddGoalCategoriesListEmpty()
+        {
 
             _service.Add(_goal.Workspace, _goal);
+        }
+
+        [TestMethod]
+        public void AddGoalCategoriesListNotEmpty()
+        {
+            Category categoryCostActive = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostInactive = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Inactive,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd = new List<Category>()
+            {
+                _category,
+                categoryCostActive,
+                categoryCostInactive
+
+            };
+            List<Category> expected = new List<Category> { categoryCostActive };
+            _service.AddCategoryToGoal(_goal, categoriesToAdd);
+            _service.Add(_goal.Workspace, _goal);
+            CollectionAssert.AreEqual(expected, _goal.Categories);
+        }
+
+        [TestMethod]
+        public void DeleteGoal()
+        {
+            _goal.Categories.Add(_category);
+            _service.Add(_workspace, _goal);
+            _service.Delete(_workspace, _goal);
+            Assert.IsNull(_service.Get(_workspace, _goal.Title));
+        }
+
+        [TestMethod]
+        public void UpdateGoalValid()
+        {
+            Category categoryCostActive = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostInactive = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Inactive,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd = new List<Category>()
+            {
+                _category,
+                categoryCostActive,
+                categoryCostInactive
+
+            };
+
+            _service.AddCategoryToGoal(_goal, categoriesToAdd);
+            _service.Add(_goal.Workspace, _goal);
+
+            Goal newGoal = new Goal
+            {
+                Title = "Home",
+                MaxAmount = 1500,
+                Workspace = _workspace
+            };
+            Category categoryCostActive2 = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostActive3 = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd2 = new List<Category>()
+            {
+                categoryCostActive2,
+                categoryCostActive3
+            };
+            List<Category> expected = new List<Category> { categoryCostActive2, categoryCostActive3 };
+            _service.AddCategoryToGoal(newGoal, categoriesToAdd2);
+            _service.Update(_goal, newGoal);
+            Assert.AreEqual(newGoal.Title, _goal.Title);
+            Assert.AreEqual(newGoal.MaxAmount, _goal.MaxAmount);
+            CollectionAssert.AreEqual(expected, _goal.Categories);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(GoalAlreadyExistsException))]
+        public void UpdateGoalAlreadyExist()
+        {
+            Category categoryCostActive = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostInactive = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Inactive,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd = new List<Category>()
+            {
+                _category,
+                categoryCostActive,
+                categoryCostInactive
+
+            };
+
+            _service.AddCategoryToGoal(_goal, categoriesToAdd);
+            _service.Add(_goal.Workspace, _goal);
+
+            Goal newGoal = new Goal
+            {
+                Title = "Home",
+                MaxAmount = 1500,
+                Workspace = _workspace
+            };
+            Category categoryCostActive2 = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostActive3 = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd2 = new List<Category>()
+            {
+                categoryCostActive2,
+                categoryCostActive3
+            };
+            Goal alreadyExistGoal = new Goal
+            {
+                Title = "Test",
+                MaxAmount = 1500,
+                Workspace = _workspace
+            };
+            _service.AddCategoryToGoal(newGoal, categoriesToAdd2);
+            _service.Add(_workspace,newGoal);
+            _service.AddCategoryToGoal(alreadyExistGoal, categoriesToAdd2);
+            _service.Update(newGoal, alreadyExistGoal);
+        }
+
+        [TestMethod]
+        public void UpdateGoalEqualName()
+        {
+            Category categoryCostActive = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostInactive = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Inactive,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd = new List<Category>()
+            {
+                _category,
+                categoryCostActive,
+                categoryCostInactive
+
+            };
+
+            _service.AddCategoryToGoal(_goal, categoriesToAdd);
+            _service.Add(_goal.Workspace, _goal);
+
+            Goal newGoal = new Goal
+            {
+                Title = "Test",
+                MaxAmount = 1500,
+                Workspace = _workspace
+            };
+            Category categoryCostActive2 = new Category
+            {
+                Name = "Test1",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            Category categoryCostActive3 = new Category
+            {
+                Name = "Test2",
+                Type = CategoryType.Cost,
+                Status = CategoryStatus.Active,
+                CreationDate = DateTime.Today.AddDays(-5),
+                Workspace = _workspace
+            };
+            List<Category> categoriesToAdd2 = new List<Category>()
+            {
+                categoryCostActive2,
+                categoryCostActive3
+            };
+            List<Category> expected = new List<Category> { categoryCostActive2, categoryCostActive3 };
+            _service.AddCategoryToGoal(newGoal, categoriesToAdd2);
+            _service.Update(_goal, newGoal);
+            Assert.AreEqual(newGoal.Title, _goal.Title);
+            Assert.AreEqual(newGoal.MaxAmount, _goal.MaxAmount);
+            CollectionAssert.AreEqual(expected, _goal.Categories);
         }
     }
 }
