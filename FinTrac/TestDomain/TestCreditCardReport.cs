9e77059891bffc52800552d51e1e4f47807d7b75
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-11 21:23:51 -0300

    feat(Domain): add credit card report

---
+++ FinTrac/TestDomain/TestCreditCardReport.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDomain
+{
+    [TestClass]
+    public class TestCreditCardReport
+    {
+        [TestInitialize]
+        public void SetUp()
+        { }
+
+        [TestMethod]
+        public void ShowCostsOfMonth() { }
+    }
+}
