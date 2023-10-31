7275d335cff1d5887cc3808d91de0bbf22613e82
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-10-31 16:17:09 -0300

    feat(goalsReport): Red goals report

---
+++ CodigoFuente/FinTrac/Domain/GoalsReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	public class GoalsReport : Report
+	{
+		public double DefinedAmount { get; set; }
+
+		public double AmountSpent { get; set; }
+
+		public bool GoalAchieved { get; set; }
+	}
+}
---
+++ CodigoFuente/FinTrac/TestDomain/TestGoalsReport.cs
@@ -0,0 +1,92 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using System.Transactions;
+using Domain;
+using Domain.Exceptions;
+
+namespace TestDomain
+{
+	[TestClass]
+	public class TestGoalsReport
+	{
+		GoalsReport goalsReport;
+
+		[TestInitialize]
+		public void SetUp()
+		{
+			goalsReport = new GoalsReport();
+		}
+		
+		[TestMethod]
+		public void CorrectDefinedAmount()
+		{
+			double amount = 15000;
+			goalsReport.DefinedAmount = amount;
+
+			Assert.AreEqual(amount, goalsReport.DefinedAmount);
+		}
+		
+		[TestMethod]
+		public void CorrectAmountSpent()
+		{
+			double amount = 9000;
+			goalsReport.AmountSpent = amount;
+
+			Assert.AreEqual(amount, goalsReport.AmountSpent);
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void NegativeDefinedAmount()
+		{
+			double negativeAmount = -1000;
+			goalsReport.DefinedAmount = negativeAmount;
+
+		}
+
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentException))]
+		public void NegativeAmountSpent()
+		{
+			double negativeAmount = -1000;
+			goalsReport.AmountSpent = negativeAmount;
+
+		}
+
+		[TestMethod]
+		public void GoalAchieved()
+		{
+			double definedAmount = 10000;
+			double amountSpent = 5000;
+
+			goalsReport.DefinedAmount = definedAmount;
+			goalsReport.AmountSpent = amountSpent;
+
+			Assert.IsTrue(goalsReport.GoalAchieved);
+		}
+
+		[TestMethod]
+		public void GoalNotAchieved()
+		{
+			double definedAmount = 9000;
+			double amountSpent = 9500;
+
+			goalsReport.DefinedAmount = definedAmount;
+			goalsReport.AmountSpent = amountSpent;
+
+			Assert.IsFalse(goalsReport.GoalAchieved);
+		}
+
+		[TestMethod]
+		public void GoalAchievedDefindedAndSpentEqual()
+		{
+			double definedAmount = 5000;
+			double amountSpent = 5000;
+
+			goalsReport.DefinedAmount = definedAmount;
+			goalsReport.AmountSpent = amountSpent;
+
+			Assert.IsTrue(goalsReport.GoalAchieved);
+		}
+	}
+}
