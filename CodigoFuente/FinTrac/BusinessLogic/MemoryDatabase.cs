c366236931f881b74f1d831a8c38b6decb33fa1a
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-10-31 23:22:14 -0300

    fixing conflicts after merge

--- CodigoFuente/FinTrac/BusinessLogic/MemoryDatabase.cs
+++ CodigoFuente/FinTrac/BusinessLogic/MemoryDatabase.cs
@@ -1,39 +1,3 @@
-<<<<<<< HEAD:FinTrac/BusinessLogic/MemoryDatabase.cs
-using Domain;
-
-namespace BusinessLogic
-{
-	public class MemoryDatabase
-	{
-		public bool isLoggedIn { get; set; } = false;
-		public List<User> Users { get; set; } = new List<User>();
-		public User currentUser { get; set; }
-		public Account currentAccount { get; set; }
-		public Workspace currentWorkspace { get; set; }
-		public MemoryDatabase()
-		{
-			Users = new List<User>();
-			addUsers();
-		}
-		public void addUsers()
-		{
-			User otro = new User
-			{
-				Name = "Admin",
-				LastName = "Test",
-				Email = "admin@test.com",
-				Password = "1234123412",
-				Address = "123"
-			};
-			Workspace defaultWorkspace = new Workspace(otro, $"Personal {otro.Name} {otro.LastName}");
-			otro.WorkspaceList.Add(defaultWorkspace);
-			Workspace workspace = new Workspace(otro, "Finanzas");
-			otro.WorkspaceList.Add(workspace);
-			Users.Add(otro);
-		}
-	}
-
-=======
 using Domain;
 
 namespace BusinessLogic
@@ -52,5 +16,4 @@ namespace BusinessLogic
 
     }
 
->>>>>>> feature/workSpace:CodigoFuente/FinTrac/BusinessLogic/MemoryDatabase.cs
 }
