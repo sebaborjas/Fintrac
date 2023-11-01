5fe047532eee534d8009920bfc1209e7afab5456
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-11-01 02:23:04 -0300

    feat(UserService): add method leave workspace

--- CodigoFuente/FinTrac/BusinessLogic/UserService.cs
+++ CodigoFuente/FinTrac/BusinessLogic/UserService.cs
@@ -1,49 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Security.Cryptography.X509Certificates;
-
-using System.Text;
-using System.Threading.Tasks;
-
 using Domain;
 using Domain.Exceptions;
 
 namespace BusinessLogic
 {
-    public class UserService
-    {
-        private readonly MemoryDatabase _memoryDatabase;
+	public class UserService
+	{
+		private readonly MemoryDatabase _memoryDatabase;
 
-        public UserService(MemoryDatabase memoryDatabase)
-        {
-            this._memoryDatabase = memoryDatabase;
-        }
+		public UserService(MemoryDatabase memoryDatabase)
+		{
+			this._memoryDatabase = memoryDatabase;
+		}
 
-        public void Add(User u)
-        {
-            if (_memoryDatabase.Users.Any(x => x.Email == u.Email))
-            {
-                throw new UserAlreadyExistsException();
-            }
-            try
-            {
-              Workspace defaultWorkspace = new Workspace(u, $"Personal {u.Name} {u.LastName}");
-              u.WorkspaceList.Add(defaultWorkspace);
-              _memoryDatabase.Users.Add(u);
-            }
-            catch (Exception exception)
-            {
-                throw exception;
-            }
+		public void Add(User u)
+		{
+			if (_memoryDatabase.Users.Any(x => x.Email == u.Email))
+			{
+				throw new UserAlreadyExistsException();
+			}
+			try
+			{
+				Workspace defaultWorkspace = new Workspace(u, $"Personal {u.Name} {u.LastName}");
+				u.WorkspaceList.Add(defaultWorkspace);
+				_memoryDatabase.Users.Add(u);
+			}
+			catch (Exception exception)
+			{
+				throw exception;
+			}
 
-        }
+		}
 
-        public User Get(string email)
-        {
-            return _memoryDatabase.Users.Find(x => x.Email == email);
-        }
+		public User Get(string email)
+		{
+			return _memoryDatabase.Users.Find(x => x.Email == email);
+		}
 
 		public User UpdateUser(String name, String email, String lastName, String address, String password)
 		{
@@ -67,24 +58,35 @@ namespace BusinessLogic
 
 
 		public void DeleteUser(User user)
-        {
-            _memoryDatabase.Users.Remove(user);
-        }
+		{
+			_memoryDatabase.Users.Remove(user);
+		}
 
-        public bool Login(string email, string password)
-        {
-            User validUser = _memoryDatabase.Users.Find(x => x.Email == email && x.Password == password);
+		public bool Login(string email, string password)
+		{
+			User validUser = _memoryDatabase.Users.Find(x => x.Email == email && x.Password == password);
 
-            if (validUser != null)
-            {
-                return true;
-            }
-            else
-            {
-                throw new InvalidUserException();
-            }
+			if (validUser != null)
+			{
+				return true;
+			}
+			else
+			{
+				throw new InvalidUserException();
+			}
+		}
 
+		public void LeaveWorkspace(User user, Workspace workspace)
+		{
+			if (user.WorkspaceList.Contains(workspace))
+			{
+				user.WorkspaceList.Remove(workspace);
+			}
+			else
+			{
+				throw new ElementNotFoundException("No se encontró el usuario");
+			}
 
-        }
-    }
+		}
+	}
 }
