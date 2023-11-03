4268a4abd501754b2b5c5bc79627da2c7929045e
Agustina Martinez <agustinamartinez1044@gmail.com>
2023-11-03 20:33:59 -0300

    feat(MemoryDatabase): add data test

--- CodigoFuente/FinTrac/BusinessLogic/MemoryDatabase.cs
+++ CodigoFuente/FinTrac/BusinessLogic/MemoryDatabase.cs
@@ -1,4 +1,5 @@
 using Domain;
+using Domain.DataTypes;
 
 namespace BusinessLogic
 {
@@ -17,7 +18,7 @@ namespace BusinessLogic
 
 		public void addUsers()
 		{
-			User otro = new User
+			User user = new User
 			{
 				Name = "Admin",
 				LastName = "Test",
@@ -25,9 +26,53 @@ namespace BusinessLogic
 				Password = "1234123412",
 				Address = "123"
 			};
-			Workspace defaultWorkspace = new Workspace(otro, $"Personal {otro.Name} {otro.LastName}");
-			otro.WorkspaceList.Add(defaultWorkspace);
-			Users.Add(otro);
+
+			Workspace defaultWorkspace = new Workspace(user, $"{user.Name} {user.LastName}");
+			user.WorkspaceList.Add(defaultWorkspace);
+			Users.Add(user);
+
+			Exchange exchange = new Exchange { Date = DateTime.Today.AddDays(-5), DollarValue = 35 };
+			defaultWorkspace.ExchangeList.Add(exchange);
+
+			Account account = new PersonalAccount { Name = "TestPersonalAccount", Currency = CurrencyType.UYU, WorkSpace = defaultWorkspace, StartingAmount = 20000 };
+			Account accountUSD = new PersonalAccount { Name = "TestPersonalAccountUSD", Currency = CurrencyType.USD, WorkSpace = defaultWorkspace, StartingAmount = 20000 };
+
+			Category category = new Category { Name = "Cosas personales", Status = CategoryStatus.Active, Type = CategoryType.Cost, Workspace = defaultWorkspace };
+			Category categoryHome = new Category { Name = "Hogar", Status = CategoryStatus.Active, Type = CategoryType.Cost, Workspace = defaultWorkspace };
+			defaultWorkspace.CategoryList.Add(category);
+			defaultWorkspace.CategoryList.Add(categoryHome);
+			List<Transaction> transactionList = new List<Transaction>();
+
+			Transaction firstTransaction = new Transaction { Title = "Gasto 1", Amount = 2000, Account = account, Category = category, Currency = CurrencyType.UYU };
+			Transaction secondTransaction = new Transaction { Title = "Gasto 2", Amount = 1000, Account = account, Category = category, Currency = CurrencyType.UYU };
+			Transaction thirdTransaction = new Transaction { Title = "Gasto 3", Amount = 1000, Account = account, Category = category, Currency = CurrencyType.UYU };
+
+			transactionList.Add(firstTransaction);
+			transactionList.Add(secondTransaction);
+			transactionList.Add(thirdTransaction);
+
+			account.TransactionList = transactionList;
+
+			defaultWorkspace.AccountList.Add(account);
+
+			List<Transaction> transactionListUSD = new List<Transaction>();
+
+			Transaction firstTransactionUSD = new Transaction { Title = "Gasto 1", Amount = 20, Currency = CurrencyType.USD, Account = accountUSD, Category = categoryHome };
+			Transaction secondTransactionUSD = new Transaction { Title = "Gasto 2", Amount = 35, Currency = CurrencyType.USD, Account = accountUSD, Category = categoryHome, };
+			Transaction thirdTransactionUSD = new Transaction { Title = "Gasto 3", Amount = 40, Currency = CurrencyType.USD, Account = accountUSD, Category = categoryHome };
+
+			transactionListUSD.Add(firstTransactionUSD);
+			transactionListUSD.Add(secondTransactionUSD);
+			transactionListUSD.Add(thirdTransactionUSD);
+
+			accountUSD.TransactionList = transactionListUSD;
+
+			defaultWorkspace.AccountList.Add(accountUSD);
+
+			Goal goal = new Goal { Title = "Ahorro", MaxAmount = 10000, Workspace = defaultWorkspace };
+			goal.Categories.Add(category);
+			goal.Categories.Add(categoryHome);
+			defaultWorkspace.GoalList.Add(goal);
 		}
 
 	}
