03ff17f6d1145c80607f27b60798f5e67ad7bc23
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-11-03 16:10:33 -0300

    feat(reports): Add Category Report class and test class

---
+++ CodigoFuente/FinTrac/TestDomain/TestCategoryReport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDomain
+{
+	[TestClass]
+	public class TestCategoryReport
+	{
+		[TestInitialize]
+		public void SetUp() { }
+
+		[TestMethod]
+		public void TestMethod() { }
+	}
+}
