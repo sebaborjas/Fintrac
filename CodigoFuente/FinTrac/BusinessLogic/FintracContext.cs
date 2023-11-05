d42254dd5305a899d976baf9314fe78f457b8874
Sebastian Borjas <sebastianborjas2000@gmail.com>
2023-11-05 20:21:23 -0300

    feat(persitentDataBase): add entity framework context

---
+++ CodigoFuente/FinTrac/BusinessLogic/FintracContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BusinessLogic
+{
+    public class FintracContext : DbContext
+    {
+        public bool isLoggedIn { get; set; } = false;
+        public DbSet<User> Users { get; set; }
+        public User currentUser { get; set; }
+        public Account currentAccount { get; set; }
+        public Workspace currentWorkspace { get; set; }
+        public FintracContext(DbContextOptions<FintracContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); 
+        }
+    }
+}
